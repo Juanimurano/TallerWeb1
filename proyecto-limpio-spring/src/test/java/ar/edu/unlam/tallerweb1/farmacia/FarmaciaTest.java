f597cf46efb7958eb7e91ef36bf579a2df21c45d
elandy2013 <elandy_2009@yahoo.com.ar>
2018-05-02 16:15:27 -0300

    Más tests en FarmaciaTest.java

--- proyecto-limpio-spring/src/test/java/ar/edu/unlam/tallerweb1/farmacia/FarmaciaTest.java
+++ proyecto-limpio-spring/src/test/java/ar/edu/unlam/tallerweb1/farmacia/FarmaciaTest.java
@@ -25,17 +25,26 @@ public class FarmaciaTest extends SpringTest {
 	private Farmacia Farmacity;
 	private Farmacia DrAhorro;
 	private Farmacia OtroFarmacity;
+	private Farmacia OtroDrAhorro;
+	private Farmacia OtraFarmacia;
 	private Direccion direccion1;
 	private Direccion direccion2;
 	private Direccion direccion3;
+	private Direccion direccion4;
+	private Direccion direccion5;
+	private Barrio constitucion;
+	private Barrio retiro;
 	private Barrio almagro;
 	private Barrio recoleta;
 	private Barrio palermo;
 	private Punto punto1;
 	private Punto punto2;
 	private Punto punto3;
+	private Punto punto4;
+	private Punto punto5;
 	private Comuna comuna1;
 	private Comuna comuna2;
+	private Comuna comuna5;
 	private Comuna comuna14;
 	private List<Farmacia> farmaciasBuscadas;
 	
@@ -45,21 +54,31 @@ public class FarmaciaTest extends SpringTest {
 		farmaciasBuscadas = new ArrayList<>();
 				
 		
-		comuna1 = new Comuna("Comuna1");
+		comuna1 = new Comuna("Comuna 1");
 		session.save(comuna1);
 		
-		almagro = new Barrio("almagro", comuna1);
+		constitucion = new Barrio("constitucion", comuna1);
+		session.save(constitucion);
+		
+		retiro = new Barrio("retiro", comuna1);
+		session.save(retiro);
+		
+		
+		comuna5 = new Comuna("Comuna 5");
+		session.save(comuna5);
+		
+		almagro = new Barrio("almagro", comuna5);//Almagro está en la comuna 5
 		session.save(almagro);
 		
 
-		comuna2 = new Comuna("Comuna2");
+		comuna2 = new Comuna("Comuna 2");
 		session.save(comuna2);
 		
 		recoleta = new Barrio("recoleta", comuna2);//Recoleta, único barrio de la comuna 2
 		session.save(recoleta);
 		
 
-		comuna14 = new Comuna("Comuna14");
+		comuna14 = new Comuna("Comuna 14");
 		session.save(comuna14);
 		
 		palermo = new Barrio("palermo", comuna14);//Misma situación que Recoleta
@@ -75,6 +94,12 @@ public class FarmaciaTest extends SpringTest {
 		direccion3 = new Direccion("Calle Cabrera", "1234", palermo);//Calle verdadera
 		session.save(direccion3);
 		
+		direccion4 = new Direccion("Avenida San Juan", "1111", constitucion);
+		session.save(direccion4);
+		
+		direccion5 = new Direccion("Juncal", "0000", retiro);
+		session.save(direccion5);
+		
 		
 		punto1 = new Punto("54,32","45,32");
 		session.save(punto1);
@@ -85,15 +110,27 @@ public class FarmaciaTest extends SpringTest {
 		punto3 = new Punto("60","90");
 		session.save(punto3);
 		
+		punto4 = new Punto("50","50");
+		session.save(punto4);
+		
+		punto5 = new Punto("50","40");
+		session.save(punto5);
+		
 		
 		Farmacity = new Farmacia("Farmacity","1234","Martes", direccion1, punto1);
 		session.save(Farmacity);
 		
-		DrAhorro = new Farmacia("Farmacity","567890","Viernes", direccion2, punto2);
+		DrAhorro = new Farmacia("Dr. Ahorro","567890","Viernes", direccion2, punto2);
 		session.save(DrAhorro);
 		
 		OtroFarmacity = new Farmacia("Farmacity","4444","Martes", direccion3, punto3);
 		session.save(OtroFarmacity);
+		
+		OtroDrAhorro = new Farmacia("Dr. Ahorro","45678901","Jueves", direccion4, punto4);
+		session.save(OtroDrAhorro);
+		
+		OtraFarmacia = new Farmacia("Azul","43210987","Lunes a Jueves", direccion5, punto5);
+		session.save(OtraFarmacia);
 	}
 	
 	@Test
@@ -101,9 +138,9 @@ public class FarmaciaTest extends SpringTest {
 	@Rollback()
 	public void buscarFarmaciasTurnoMartes() {
 		farmaciasBuscadas = session
-				.createCriteria(Farmacia.class)
-				.add(Restrictions.eq("diaDeTurno","Martes"))
-				.list();
+			.createCriteria(Farmacia.class)
+			.add(Restrictions.eq("diaDeTurno","Martes"))
+			.list();
 		
 		assertThat(farmaciasBuscadas.size()).isEqualTo(1);//¡Si hay una sola farmacia, no más de una, sino falla!
 	}
@@ -113,10 +150,10 @@ public class FarmaciaTest extends SpringTest {
 	@Rollback()
 	public void buscarFarmaciasMismaCalle() {
 		farmaciasBuscadas = session
-				.createCriteria(Farmacia.class)
-				.createAlias("direccion", "direccionBuscada")
-				.add(Restrictions.eq("direccionBuscada.calle", "Avenida Pipi"))
-				.list();
+			.createCriteria(Farmacia.class)
+			.createAlias("direccion", "direccionBuscada")
+			.add(Restrictions.eq("direccionBuscada.calle", "Avenida Pipi"))
+			.list();
 		
 		assertThat(farmaciasBuscadas.size()).isEqualTo(1);
 	}
@@ -126,12 +163,11 @@ public class FarmaciaTest extends SpringTest {
 	@Rollback()
 	public void buscarFarmaciasMismoBarrio() {
 		farmaciasBuscadas = session
-				.createCriteria(Farmacia.class)
-				.createAlias("direccion", "direccionBuscada")
-				.createAlias("direccionBuscada.barrio", "barrioBuscado")
-				.add(Restrictions.eq("barrioBuscado.Nombre", "almagro"))
-				.list();
-		
+			.createCriteria(Farmacia.class)
+			.createAlias("direccion", "direccionBuscada")
+			.createAlias("direccionBuscada.barrio", "barrioBuscado")
+			.add(Restrictions.eq("barrioBuscado.Nombre", "almagro"))
+			.list();
 		
 		assertThat(farmaciasBuscadas.size()).isEqualTo(1);
 	}
@@ -141,12 +177,11 @@ public class FarmaciaTest extends SpringTest {
 	@Rollback()
 	public void buscarFarmaciasRecoleta() {
 		farmaciasBuscadas = session
-				.createCriteria(Farmacia.class)
-				.createAlias("direccion", "direccionBuscada")
-				.createAlias("direccionBuscada.barrio", "barrioBuscado")
-				.add(Restrictions.eq("barrioBuscado.Nombre", "recoleta"))
-				.list();
-		
+			.createCriteria(Farmacia.class)
+			.createAlias("direccion", "direccionBuscada")
+			.createAlias("direccionBuscada.barrio", "barrioBuscado")
+			.add(Restrictions.eq("barrioBuscado.Nombre", "recoleta"))
+			.list();
 		
 		assertThat(farmaciasBuscadas.size()).isGreaterThanOrEqualTo(1);//Así cuando hay dos o más farmacias en el mismo barrio este test no falle
 	}
@@ -156,9 +191,39 @@ public class FarmaciaTest extends SpringTest {
 	@Rollback()
 	public void buscarTodasLasFarmacity() {
 		farmaciasBuscadas = session
-				.createCriteria(Farmacia.class)
-				.add(Restrictions.eq("nombre","Farmacity"))
-				.list();
+			.createCriteria(Farmacia.class)
+			.add(Restrictions.eq("nombre","Farmacity"))
+			.list();
+		
+		assertThat(farmaciasBuscadas.size()).isGreaterThanOrEqualTo(1);
+	}
+	
+	@Test
+	@Transactional
+	@Rollback()
+	public void buscarFarmaciasMismaComuna() {//Sí, de la Comuna 1
+		farmaciasBuscadas = session
+			.createCriteria(Farmacia.class)
+			.createAlias("direccion", "direccionBuscada")
+			.createAlias("direccionBuscada.barrio", "barrioBuscado")
+			.createAlias("barrioBuscado.comuna", "comunaBuscada")
+			.add(Restrictions.eq("comunaBuscada.nombre", "Comuna 1"))
+			.list();
+		
+		assertThat(farmaciasBuscadas.size()).isGreaterThanOrEqualTo(1);
+	}
+	
+	@Test
+	@Transactional
+	@Rollback()
+	public void buscarFarmacityEnPalermo() {
+		farmaciasBuscadas = session
+			.createCriteria(Farmacia.class)
+			.add(Restrictions.eq("nombre","Farmacity"))
+			.createAlias("direccion", "direccionBuscada")
+			.createAlias("direccionBuscada.barrio", "barrioBuscado")
+			.add(Restrictions.eq("barrioBuscado.Nombre", "palermo"))
+			.list();
 		
 		assertThat(farmaciasBuscadas.size()).isGreaterThanOrEqualTo(1);
 	}
